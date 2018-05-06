bf4ede80c2bd2b6f0aaa1c6d485d0591891919b9
tonghong02 <tonghong03@gmail.com>
2018-05-06 12:45:49 +0700

    test pagination

--- recommendation-movie-system/src/app/pages/phim-bo/phim-bo.component.ts
+++ recommendation-movie-system/src/app/pages/phim-bo/phim-bo.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+import { UserService } from '../../services/user.service';
+import { ContactModel } from '../../models/contact.model';
+import { PaginationService } from '../../services/pagination.service';
+import * as _ from 'underscore';
+
 
 @Component({
   selector: 'app-phim-bo',
@@ -7,9 +16,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhimBoComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+  private allItems: any[];
+
+  // pager object
+  pager: any = {};
+
+  // paged items
+  pagedItems: any[];
+  constructor(
+    private _user: UserService,
+    private _router: Router,
+    private fb: FormBuilder,
+    private _toastr: ToastrService,
+    private _pagination: PaginationService
+  ) { }
 
   ngOnInit() {
+    this.listContact();
   }
+
+  listContact() {
+    this._user
+      .getListUser()
+      .subscribe(data => {
+        console.log(data);
+        // set items to json response
+        this.allItems = data;
+
+        // initialize to page 1
+        this.setPage(1);
+      })
+  }
+
+  setPage(page: number) {
+    if (page < 1 || page > this.pager.totalPages) {
+      return;
+    }
+
+    // get pager object from service
+    this.pager = this._pagination.getPager(this.allItems.length, page);
+
+    // get current page of items
+    this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
+  }
+
 
 }
